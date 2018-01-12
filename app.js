acc174da0a096fdfcf65c29e62b3e74f7be8bd12
Daniel Lisboa <daniellisboa.me@gmail.com>
2018-01-11 22:22:49 -0200

    Criando servidor

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('Sistema node ...');
+
+const http = require('http')
+const port = 3000
+const ip = 'localhost'
+ 
+const server = http.createServer((req, res) => {
+  console.log('Recebendo uma request!')
+  res.end('<h1>Resposta!</h1>')
+})
+ 
+server.listen(port, ip, () => {
+  console.log(`Servidor rodando em http://${ip}:${port}`)
+})
