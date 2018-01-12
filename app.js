3bb1cecf6cabbeeef17eea135d1d47db3a443411
danlisboa <daniellisboa.me@gmail.com>
2018-01-11 23:43:23 -0200

    pontuação

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 console.log('Sistema node ...');
 
-const http = require('http')
-const port = 3000
-const ip = 'localhost'
- 
-const server = http.createServer((req, res) => {
-  console.log('Recebendo uma request!')
-  res.end('<h1>Resposta!</h1>')
-})
- 
+var http = require('http');
+var port = 3000;
+var ip = 'localhost';
+
+var server = http.createServer((req, res) => {
+  console.log('Recebendo uma request!');
+  res.end('<h1>Resposta!</h1>');
+});
+
 server.listen(port, ip, () => {
-  console.log(`Servidor rodando em http://${ip}:${port}`)
-})
+  console.log(`Servidor rodando em http://${ip}:${port}`);
+});
